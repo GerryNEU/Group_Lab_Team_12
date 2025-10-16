073f592678dbec42ecc6386ada6f91becb93745a
Cris77ovo <ouyang.ka@northeastern.edu>
2025-10-16 16:31:21 -0400

    greet3

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Member1.main(args);
+
+        Member4.main(args);
+
+        Member2.main(args);
+
+        Member3.main(args);
+
+    }
+}
