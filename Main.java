37e6e5291ee478ccad44b6d7a96cbb72db70d633
Hugo Yang <yang.jiay@northeastern.edu>
2025-10-16 16:17:05 -0400

    update main (#8)
    * Create Member4.java
    
    * Update Main.java
    
    ---------
    
    Co-authored-by: Zhizhi Li <li.zhizhi@northeastern.edu>

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Member1.main(args);
+
+        Member4.main(args);
+
         Member2.main(args);
+
     }
 }
